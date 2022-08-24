eb03c7a9d4797d286fe7beb33f4132d108c91fec
unnati098 <100790972+unnati098@users.noreply.github.com>
2022-08-24 09:46:11 -0400

    Add files via upload

--- Automated_testing/src/class1/Testing1.java
+++ Automated_testing/src/class1/Testing1.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -11,7 +12,7 @@ import org.openqa.selenium.interactions.Actions;
 import graphql.Assert;
 
 public class Testing1 {
-
+	static WebDriver driver;
 	@Test
 	public void test() throws InterruptedException {
 		//fail("Not yet implemented");
@@ -19,5 +20,17 @@ public class Testing1 {
 		m1.testing();
 	}
 	
+	@Test
+	public void create() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\2093906\\Desktop\\chromedriver.exe");
+		 driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void test1() {
+		//fail("Not yet implemented");
+		create();
+		driver.get("https://www.google.com/");
+	}
 
 }
